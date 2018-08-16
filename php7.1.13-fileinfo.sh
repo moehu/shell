7074fea589b883f064f3b339138d6da53a38be88
尐酱油 <scncwqs@126.com>
2018-08-16 16:40:39 +0800

    Update php7.1.13-fileinfo.sh

--- php7.1.13-fileinfo.sh
+++ php7.1.13-fileinfo.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 PREFIX="/vhs/kangle/ext/tpl_php7113"
-wget wget -c https://raw.githubusercontent.com/moehu/kangle/master/php/7.1/7113/php-7.1.13.tar.bz2 -O php-7.1.13.tar.bz2
+wget wget -c https://raw.githubusercontent.com/1265578519/kangle/master/php/7.1/7113/php-7.1.13.tar.bz2 -O php-7.1.13.tar.bz2
 tar xjf php-7.1.13.tar.bz2
-cd /php-7.1.13/ext/fileinfo
+cd /root/php-7.1.13/ext/fileinfo
 /vhs/kangle/ext/tpl_php7113/bin/phpize
 ./configure -with-php-config=/vhs/kangle/ext/tpl_php7113//bin/php-config --enable-fileinfo
 make && make install
 cd /vhs/kangle/ext/tpl_php7113
 rm -rf php-templete.ini
-wget https://raw.githubusercontent.com/moehu/kangle/master/php/7.1/7113/php-templete.ini -O $PREFIX/php-templete.ini
+wget https://raw.githubusercontent.com/moehu/kangle/master/php/php7.1/71113/php-templete.ini
 /vhs/kangle/bin/kangle -r
